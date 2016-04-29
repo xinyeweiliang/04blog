d26d6a43b67851a7409a4477ddb9f72a0eeed982
xinyeweiliang <1078270788@qq.com>
2016-04-29 18:27:37 +0800

    submit

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var articles = require('./routes/articles');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
+app.engine('html',require('ejs').__express);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -24,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/articles', articles);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/*用户注册*/
+router.get('/reg', function(req, res) {
+  res.render('user/reg', {title: '注册'});
 });
+/*用户提交注册信息的处理*/
+router.post('/reg', function (req, res) {
+
+});
+/*显示用户登录表单*/
+router.get('/login', function (req, res) {
+  res.render('user/login', {title: '登录'});
+});
+/*用户提交登录信息的处理*/
+router.post('/login', function (req, res) {
+
+});
+router.get('/logout', function (req, res) {
+
+});
+
+
 
 module.exports = router;
